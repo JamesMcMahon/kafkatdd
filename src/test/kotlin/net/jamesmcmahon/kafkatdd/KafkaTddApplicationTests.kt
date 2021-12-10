0fda464857b8edbdcf4b7ee0d8b07bb46e2d5825
James McMahon <mcmahonja@vmware.com>
2021-12-10 11:57:42 -0600

    Better capitalization of main class

--- src/test/kotlin/net/jamesmcmahon/kafkatdd/KafkaTddApplicationTests.kt
+++ src/test/kotlin/net/jamesmcmahon/kafkatdd/KafkaTddApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KafkatddApplicationTests {
+class KafkaTddApplicationTests {
 
 	@Test
 	fun contextLoads() {
